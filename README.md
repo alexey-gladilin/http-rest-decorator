378451025b2d225515cbf3e42c9b3a247467c541
alexey.gladilin <alexey.gladilin@yandex.ru>
2019-03-02 08:16:56 +0300

    build: project initialization

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # http-rest-decorator
+
+This library allows to interact with rest api in your angular app.
---
+++ projects/http/src/public_api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of http
+ */
+
+export * from './lib/http.service';
+export * from './lib/http.module';
