c6dd0e8bb153f3b46c3c30a4e42ec2de5ffab305
alexey.gladilin <alexey.gladilin@yandex.ru>
2019-03-09 08:26:52 +0300

    test: added webApi service for demo application

---
+++ projects/demoApp/src/app/services/employee.webapi.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from 'http-rest-decorator';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeWebApi extends HttpService {
+
+}
