8392a212a15db3b1970c106c2b9a3367efb16ff3
alexey.gladilin <alexey.gladilin@yandex.ru>
2019-03-13 07:56:03 +0300

    feat(demo): added http service configuration example

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -5,6 +5,22 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { HeroesModule } from './heroes/heroes.module';
 import { HttpClientModule } from '@angular/common/http';
+import {
+  HTTP_CONFIG,
+  HttpConfig
+} from 'http-rest-decorator';
+
+/**
+ * default http service configuration settings
+ */
+export const DEFAULT_HTTP_CONFIG: HttpConfig = {
+  Url: 'api.heroes.com',
+  Headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'DataType': 'application/json'
+  }
+};
 
 /**
  * main component application
@@ -20,7 +36,7 @@ import { HttpClientModule } from '@angular/common/http';
     HeroesModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_CONFIG, useValue: DEFAULT_HTTP_CONFIG }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
