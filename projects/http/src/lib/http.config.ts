e3a3077df9f040d1a5d7ce486661ad802368b07c
alexey.gladilin <alexey.gladilin@yandex.ru>
2019-03-13 09:05:51 +0300

    refactor(http-rest-decorator): made an optional configuration parameter

--- projects/http/src/lib/http.config.ts
+++ projects/http/src/lib/http.config.ts
@@ -2,6 +2,8 @@
  * http service configuration settings data model
  */
 export interface HttpConfig {
+  /** webApi host url */
   Url: string;
-  Headers: string | { [name: string]: string | string[] };
+  /** http headers */
+  Headers?: string | { [name: string]: string | string[] };
 }
